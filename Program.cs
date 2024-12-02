a9291e1f224fafba0097a75084d1309210a4a66b
Kamil Kondziolka <wono83@interia.pl>
2024-12-02 18:36:48 +0100

    Second day of

---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using var inputReader = new StreamReader(Path.Join(Directory.GetCurrentDirectory(), "input.txt"));
+
+int part1Answer = 0, part2Answer = 0;
+while (!inputReader.EndOfStream)
+{
+    var line = await inputReader.ReadLineAsync();
+    var reports = line!.Split(" ");
+    if (IsSafe(reports))
+    {
+        part1Answer++;
+    }
+    else
+    {
+        for (int i = 0; i < reports.Length; i++)
+        {
+            if (IsSafe(reports, i))
+            {
+                part2Answer++;
+                break;
+            }
+        }
+    }
+}
+Console.WriteLine($"First part: {part1Answer}");
+Console.WriteLine($"Second part: {part1Answer + part2Answer}");
+return;
+
+bool IsSafe(string[] reports, int skipIndex = -1)
+{
+    int left = 0, right = 1, corrNegDiff = 0, corrPosDiff = 0;
+    while (right < reports.Length)
+    {
+        if (left == skipIndex)
+        {
+            left = right;
+            right++;
+        }
+
+        if (right == skipIndex)
+        {
+            right++;
+            if (right == reports.Length)
+            {
+                break;
+            }
+        }
+
+        int report1 = int.Parse(reports[left]), report2 = int.Parse(reports[right]);
+        var diff = report1 - report2;
+        if (Math.Abs(diff) > 0 && Math.Abs(diff) <= 3)
+        {
+            if (diff < 0)
+            {
+                corrNegDiff++;
+            }
+            else
+            {
+                corrPosDiff++;
+            }
+        }
+        left = right;
+        right++;
+    }
+    if(skipIndex == -1)
+    {
+        if (corrPosDiff == reports.Length - 1 || corrNegDiff == reports.Length - 1)
+        {
+            return true;
+        }
+    }
+    else
+    {
+        if (corrPosDiff == reports.Length - 2 || corrNegDiff == reports.Length - 2)
+        {
+            return true;
+        }
+    }
+    return false;
+}
